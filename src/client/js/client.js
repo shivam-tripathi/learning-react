ce898b80c40ea1bd3d9be1ff21bcba4381fb31d8
Shivam Tripathi <shivam.flash@gmail.com>
2018-02-06 01:52:09 +0530

    Reset directory structure, rewrite run scripts

---
+++ src/client/js/client.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+const HelloWorld = () => <h1>Hello, World!</h1>;
+
+render(
+    <HelloWorld/>,
+    document.getElementById('react-container')
+);
---
+++ src/server/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import path from 'path';
+
+// Create app
+const app = express();
+
+// Support both pug and html rendering
+app.set('view engine', 'pug');
+app.set('view engine', 'html');
+app.engine('pug', require('pug').__express);
+app.engine('html', require('ejs').renderFile);
+
+// Set views section, which contains all the html and pug files
+app.set('views', path.join(__dirname, '/../../src/views'));
+// Client side css and js files which would be loaded in the browser
+app.use(express.static(path.join(__dirname + '/../client')));
+
+// Express routing
+app.get('/', (req, res, next) => {
+	res.render('index.pug');
+});
+
+// Listen
+app.listen('8888', '127.0.0.1');
+console.log('Express started on port 8888');
+console.log('All the best!');
+
+
